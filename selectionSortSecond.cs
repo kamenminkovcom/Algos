18e66ec1ce8b99c88d6eed26b4c840982e04e689
Kamen Minkov <kamenminkov.com@abv.bg>
2017-02-12 00:08:54 +0200

    Add files via upload

---
+++ selectionSortSecond.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class SelectionSort
+{
+    private static int[] collection = new int[] { 1, 5, 1, 3, 5, 10, 10, 100, 199, 45, 51 };
+
+    static void Main()
+    {
+        for (int i = 0; i < collection.Length - 1; i++)
+        {
+            for (int j = i + 1; j < collection.Length; j++)
+            {
+                if (collection[j] < collection[i])
+                {
+                    // Swap elements
+                    collection[i] = collection[j] - collection[i];
+                    collection[j] = collection[j] - collection[i];
+                    collection[i] = collection[i] + collection[j];
+                }
+            }
+
+        }
+        foreach (var element in collection)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
+
